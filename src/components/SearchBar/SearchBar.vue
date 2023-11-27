1fef28134fbeb8c86828f82faee06968fcda6c3d
bino <bonoself@gmail.com>
2023-11-27 21:51:55 +0900

    feat: add searchbar ui

---
+++ src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,40 @@
+<script lang="ts" setup>
+import { VueSpinnerTail } from 'vue3-spinners';
+
+const text = defineModel<string>({ required: true });
+const submit = () => {
+  console.log('z'); /* @DELETE  */
+};
+</script>
+<template>
+  <form
+    class="max-w-xl h-[60px] relative shadow-md rounded-2xl ring-1 ring-[#dedede7c] overflow-hidden bg-white"
+    @submit.prevent="submit"
+  >
+    <input
+      v-model="text"
+      :maxlength="250"
+      class="w-full h-full px-5 outline-none placeholder:text-black/20"
+      placeholder="검색어를 입력해주세요"
+    >
+    <div class="absolute right-0 top-0 bottom-0 flex">
+      <div
+        :class="text.length === 0 ? 'opacity-0' : 'opacity-20'"
+        class="input-element"
+      >
+        <VueSpinnerTail :size="30" />
+      </div>
+      <div
+        :class="text.length === 0 ? 'scale-90 text-opacity-10' : 'scale-105 text-opacity-70'"
+        class="input-element"
+      >
+        &#x2716;
+      </div>
+    </div>
+  </form>
+</template>
+<style lang="scss" scoped>
+.input-element {
+    @apply w-[50px] h-full flex items-center justify-center text-black transition-all ease-out
+}
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,21 @@
 <script lang="ts" setup>
-import { greeting } from '@global/index';
+import SearchBar from 'src/components/SearchBar/SearchBar.vue';
+import { ref } from 'vue';
 
+const searchText = ref('');
+const searchBar = ref<typeof SearchBar>();
 </script>
 <template>
-  {{ greeting }}
+  <div class="bg-slate-100">
+    <div class="py-10">
+      {{ searchBar?.__defaults }}
+      <SearchBar
+        ref="searchBar"
+        v-model="searchText"
+        class="m-auto"
+      />
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
 </style>
