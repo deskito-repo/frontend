65152683f8924cd731f64ac79e5f394f13eeddce
bino <bonoself@gmail.com>
2023-12-02 16:44:13 +0900

    feat: update scrollbar

--- src/components/templates/Home/index.vue
+++ src/components/templates/Home/index.vue
@@ -3,7 +3,10 @@ import Header from './Header.vue';
 import Widgets from './Widgets.vue';
 </script>
 <template>
-  <div class="bg-slate-100">
+  <div
+    class="bg-slate-100"
+    style="overflow-y: auto"
+  >
     <div class="max-w-7xl m-auto">
       <div class="mb-10">
         <Header />
