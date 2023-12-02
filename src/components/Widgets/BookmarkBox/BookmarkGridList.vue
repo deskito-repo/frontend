d0e48c71e41820b7f844260cbbef54ee14a8d0fb
bino <bonoself@gmail.com>
2023-12-02 10:47:51 +0900

    feat: add boilerplate of bookmarks

---
+++ src/components/Widgets/BookmarkBox/BookmarkGridList.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+</script>
+<template>
+  <BookmarkRow
+    v-for="_ in 12"
+    :key="_"
+  />
+</template>
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Widgets/BookmarkBox/index.vue
@@ -0,0 +1,9 @@
+<script lang="ts" setup>
+import BookmarkGridList from './BookmarkGridList.vue';
+
+</script>
+<template>
+  <BookmarkGridList />
+</template>
+<style lang="scss" scoped>
+</style>
