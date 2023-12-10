b50394300cd0c187545b782c822762a0ca405a48
bino <bonoself@gmail.com>
2023-12-10 16:09:17 +0900

    feat(useModal): add history trap

--- src/composables/useModal.ts
+++ src/composables/useModal.ts
@@ -1,18 +1,27 @@
+import { HistoryTrap } from 'src/utils/HistoryTrap';
 import {
   useModal as useFinalModal,
 } from 'vue-final-modal';
 
 type Props = Parameters<typeof useFinalModal>[0];
 export const useModal = (options?: Props) => {
+  let historyTrap: HistoryTrap;
   const { close, open } = useFinalModal({
     ...options,
     attrs: {
       ...options?.attrs,
-      onClose: () => close(),
+      onClose: () => {
+        historyTrap.catch();
+        return close();
+      },
     },
   });
   return {
-    open,
+    open: () => {
+      historyTrap = new HistoryTrap('modal');
+      historyTrap.onCatch(close);
+      return open();
+    },
     close,
   };
 };
