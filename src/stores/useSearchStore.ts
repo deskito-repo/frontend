81a896723759324b4fba15e07391711870b744af
bino <bonoself@gmail.com>
2023-12-14 22:14:22 +0900

    feat: enter to website if search query is full url

--- src/stores/useSearchStore.ts
+++ src/stores/useSearchStore.ts
@@ -3,8 +3,18 @@ import { ref } from 'vue';
 
 export const useSearchStore = defineStore('search', () => {
   const searchText = ref('');
+
+  const isHttpRequest = (url: string) => {
+    const protocols = ['http://', 'https://'];
+    return protocols.some((p) => url.startsWith(p));
+  };
+  const isSearchWebSite = (url: string) => isHttpRequest(url) || url.startsWith('www.');
   const search = (query: string) => {
     searchText.value = '';
+    if (isSearchWebSite(query)) {
+      window.location.href = isHttpRequest(query) ? query : `https://${query}`;
+      return;
+    }
     window.location.href = `https://www.google.com/search?q=${query}`;
   };
   return {
