ae1475ba45ba064b31e33ed05bd789b12bdc27d2
bino <bonoself@gmail.com>
2023-12-14 23:23:34 +0900

    feat: add cursor to suggestionList

--- src/components/elements/Suggestion/SuggestionList.vue
+++ src/components/elements/Suggestion/SuggestionList.vue
@@ -4,9 +4,32 @@ import CalculateIcon from 'src/components/common/icons/CalculateIcon.vue';
 import TextIcon from 'src/components/common/icons/TextIcon.vue';
 import { useSearchStore } from 'src/stores/useSearchStore';
 import { useSuggestionStore } from 'src/stores/useSuggestionStore';
+import { useMagicKeys, whenever } from '@vueuse/core';
+import { ref } from 'vue';
 
+const listRef = ref<HTMLLIElement[]>([]);
 const searchStore = useSearchStore();
 const { list } = storeToRefs(useSuggestionStore());
+
+const { arrowdown, arrowup } = useMagicKeys();
+const cursor = ref<null | number>(null);
+const cursorManager = {
+  move(isUp: boolean) {
+    cursor.value = cursor.value === null ? 0 : cursor.value + (isUp ? 1 : -1);
+    if (cursor.value === null) {
+      return;
+    }
+    listRef.value.at(cursor.value)?.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  },
+  up() { cursorManager.move(true); },
+  down() { cursorManager.move(false); },
+};
+whenever(
+  () => searchStore.searchText,
+  () => cursor.value = null,
+);
+whenever(arrowdown, cursorManager.up);
+whenever(arrowup, cursorManager.down);
 </script>
 <template>
   <div class="bg-primary bg-opacity-10">
@@ -16,7 +39,9 @@ const { list } = storeToRefs(useSuggestionStore());
     >
       <li
         v-for="{ type, text }, key in list"
+        ref="listRef"
         :key="key"
+        :class="key === cursor && 'selected'"
         class="px-4 h-[50px] leading-[50px] transition-all duration-100 hover:bg-primary/10 opacity-70 ease-out hover:opacity-100 cursor-pointer"
         @click="searchStore.search(text)"
       >
@@ -48,5 +73,8 @@ const { list } = storeToRefs(useSuggestionStore());
 }
 .text {
   @apply text-ellipsis whitespace-nowrap overflow-hidden flex-1;
+}
+.selected {
+  @apply bg-primary/10 opacity-100;
 }
 </style>
