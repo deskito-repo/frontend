cde833acf2de0de014a6151f288807bd4651958a
bino <bonoself@gmail.com>
2023-12-14 22:30:57 +0900

    chore(vite.config): add dotenv vars

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import * as path from 'path';
 import Components from 'unplugin-vue-components/vite';
@@ -6,61 +6,67 @@ import { ElementPlusResolver } from 'unplugin-vue-components/resolvers';
 import { VantResolver } from '@vant/auto-import-resolver';
 import copyFiles from 'rollup-plugin-copy';
 
+const envPrefixes = ['APP_'];
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://monolith:4124',
-        changeOrigin: true,
-        secure: false,
-        ws: true,
+export default defineConfig(({ mode }) => {
+  const {
+    APP_MONOLITHIC_URL = '',
+  } = { ...loadEnv(mode, process.cwd(), envPrefixes) };
+  return {
+    server: {
+      proxy: {
+        '/api': {
+          target: APP_MONOLITHIC_URL,
+          changeOrigin: true,
+          secure: false,
+          ws: true,
+        },
       },
     },
-  },
-  plugins: [
-    vue({
-      script: {
-        defineModel: true,
-      },
-    }),
-    Components({
-      resolvers: [
-        VantResolver(),
-        ElementPlusResolver(),
-      ],
-    }),
-
-    copyFiles({
-      targets: [
-        {
-          src: './manifest.json',
-          dest: 'dist',
+    plugins: [
+      vue({
+        script: {
+          defineModel: true,
         },
-      ],
-      hook: 'writeBundle',
-    }),
-    /**
+      }),
+      Components({
+        resolvers: [
+          VantResolver(),
+          ElementPlusResolver(),
+        ],
+      }),
+
+      copyFiles({
+        targets: [
+          {
+            src: './manifest.json',
+            dest: 'dist',
+          },
+        ],
+        hook: 'writeBundle',
+      }),
+      /**
      * as using sqlite, it needed to allow OPFS
      */
-    {
-      name: 'configure-response-headers',
-      configureServer: (server) => {
-        server.middlewares.use((_req, res, next) => {
-          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
-          next();
-        });
+      {
+        name: 'configure-response-headers',
+        configureServer: (server) => {
+          server.middlewares.use((_req, res, next) => {
+            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+            next();
+          });
+        },
+      },
+    ],
+    optimizeDeps: {
+      exclude: ['sqlocal'],
+    },
+    resolve: {
+      alias: {
+        '@global': path.join(__dirname, './global'),
+        src: path.join(__dirname, './src'),
       },
     },
-  ],
-  optimizeDeps: {
-    exclude: ['sqlocal'],
-  },
-  resolve: {
-    alias: {
-      '@global': path.join(__dirname, './global'),
-      src: path.join(__dirname, './src'),
-    },
-  },
+  };
 });
