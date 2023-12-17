c4896e92cb05a345d42bfa119c969fd01763d77f
bino <bonoself@gmail.com>
2023-12-17 17:36:21 +0900

    perf:

--- libs/vue3-effect-directive/src/composables/useGlobalOption.ts
+++ libs/vue3-effect-directive/src/composables/useGlobalOption.ts
@@ -1,6 +1,6 @@
 import { Options } from 'src/types';
 
-const options: Options = Object();
+const options: Options = Object.create({});
 export const useGlobalOption = () => ({
   assign: (newOptions: Options): void => {
     Object.assign(options, newOptions);
