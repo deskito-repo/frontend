0dc627c6ea73d1f939f1c0b23f40c87518be7efd
bino <bonoself@gmail.com>
2024-02-02 16:45:16 +0900

    chore(vite.config.ts): remove cors header

--- vite.config.ts
+++ vite.config.ts
@@ -56,8 +56,8 @@ export default defineConfig(({ mode }) => {
         name: 'configure-response-headers',
         configureServer: (server) => {
           server.middlewares.use((_req, res, next) => {
-            res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
-            res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+            // res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+            // res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
             next();
           });
         },
