e29d80f7082158e17d26851c5ec96e4e75d683ec
bino <bonoself@gmail.com>
2024-02-02 16:44:50 +0900

    feat(BookmarkRow):

--- src/components/elements/BookmarkBox/BookmarkRow.vue
+++ src/components/elements/BookmarkBox/BookmarkRow.vue
@@ -1,7 +1,22 @@
 <script lang="ts" setup>
+export type Props = {
+  src: string;
+  title: string;
+};
+defineProps<Props>();
 </script>
 <template>
-  <div></div>
+  <div class="hover:bg-text hover:bg-opacity-5 transition-colors cursor-pointer p-2 rounded-md">
+    <div class="flex flex-col items-center gap-1">
+      <img
+        v-bind="{ src }"
+        class="w-14"
+      >
+      <div>
+        <span class="text-sm">{{ title }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 <style lang="scss" scoped>
 </style>
